f180519b43abaf86ab03a6fcb11c3971a5c72e42
Emilia Kaihua <n0kama00@students.oamk.fi>
2021-09-13 20:08:01 +0300

    Valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import fillOptions from './fillOptions';
 
 function App() {
+
+  const [brutto, setBrutto] = useState(0);
+  const [vero, setVero] = useState(1);
+  const [vakuutus, setVakuutus] = useState(1);
+  const [elake, setElake] = useState (1);
+  const [netto, setNetto] = useState(0);
+  
+  const [veroSumma, setVeroSumma] = useState(0);
+  const [elakeSumma, setElakeSumma] = useState(0);
+  const [vakuutusSumma, setVakuutusSumma] = useState(0);
+
+
+  function calculate(e){
+    e.preventDefault();
+    const veroEurot = brutto / 100 * vero;
+    const elakeEurot = brutto / 100 * elake;
+    const vakuutusEurot = brutto / 100 * vakuutus;
+    setVeroSumma(veroEurot);
+    setElakeSumma(elakeEurot);
+    setVakuutusSumma(vakuutusEurot);
+    setNetto(brutto - veroEurot - elakeEurot - vakuutusEurot);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div id="container">
+      <h3>Salary calculator</h3>
+      <form onSubmit={calculate}>
+        <div>
+          <label>Salary</label>
+        </div>
+        <input type="number" value={brutto}
+        onChange={e=> setBrutto(e.target.value)}/>
+
+        <div>
+          <label>Tax percentage (%) </label>
+          <output>{veroSumma} €</output>
+        </div>
+        <input type="number" value={vero}
+        onChange={e=> setVero(e.target.value)}/>
+
+        <div>
+          <label>Pension (%) </label>
+          <output>{elakeSumma} €</output>
+        </div>
+        <select value={elake}
+        onChange={e=> setElake(e.target.value)}>
+          <fillOptions />
+        </select>
+
+        <div>
+          <label>Insurance (%) </label>
+          <output>{vakuutusSumma} €</output>
+        </div>
+        <select value={vakuutus}
+        onChange={e=> setVakuutus(e.target.value)}>
+          <fillOptions />
+        </select>
+
+        <div>
+          <label>Your salary after payments</label>
+          <output id="netto">{netto} €</output>
+        </div>
+        <button>Calculate</button>
+        </form>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/fillOptions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function fillOptions(){
+    const percentages = [];
+    for (let i = 0; i <=5; i = 1 + 0,25){
+        percentages.push(i);
+    }
+
+    return (
+        percentages.map(percentage => {
+            return <option value={percentage}>{percentage.toFixed(2)} %</option>
+        })
+    )
+}
